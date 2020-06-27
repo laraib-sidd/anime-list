abd9412a5e4b79dcfcef1544e6715c40154c0db6
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-27 22:53:12 +0530

    file upload

---
+++ src/anime.js
@@ -0,0 +1,347 @@
+export const anime =[
+	{
+		"title": "Akira",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqDAYEsm8YcNQCLpdRdr0UUWgbMWC7QHh3ytQXvwvYKA8-Vyaj_0tQBrEQ0vs&s"
+	},
+	{
+		"title": "Boku no Hero Academia",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6X_n-fu9rzc4nBG8XAfq_1B_W8Us1IuFVVKEsi_iAt0hSgxVrnC21uNKZbyQ&s"
+	},
+	{
+		"title": "Boku no Hero Academia 2nd Season",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT3RTFLDmJA2jjP5kiKxJ9luQpQ7j3kksY-TMzkgC6BP9GNf6a64EQ_Fo5Rms&s"
+	},
+	{
+		"title": "Boku no Hero Academia 2nd Season: Hero Note",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAOMM3Fdc5Z39rmIc_5k1oQztt0Zo7I96dhxZ_N2LLW4la9LjzcNUSJWK3KA&s"
+	},
+	{
+		"title": "Boku no Hero Academia 3rd Season",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIHomQIiQHx4PnkV1CQaZdbJPOk_Y1P8NSjMwpFG3nLth-mISZGKPJpFDwog&s"
+	},
+	{
+		"title": "Boku no Hero Academia 4th Season",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzK-9X1pCTeycW5iERtk1ly8v1ekKVKmdqAJttRjItSyyol2wUZTA3X2aLpA&s"
+	},
+	{
+		"title": "Clannad",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRndG72TAgy4W-HA5AasGtmviA6JLUlR5NCyUi_cP67j65NqgmVk86mALSIYsI&s"
+	},
+	{
+		"title": "Clannad: After Story",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgNkSn-uje9WyRVcUDVdek3oB-cEkRKkxMFGunxWBhF7Dc34Y1tfQ070Hs2Vs&s"
+	},
+	{
+		"title": "Code Geass: Hangyaku no Lelouch",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvx6hDT-GpHVsLwpIjZ_XtQk5p9jhAMRnLmVOBP4bl0dUHMzoDZSWptoupEw&s"
+	},
+	{
+		"title": "Code Geass: Hangyaku no Lelouch R2",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD6xcWoSHoHZGPCiOTbF9_qhjQbr2uAFFTnsrj3ahvtwmJQ7Eqjp5sls_C5o4&s"
+	},
+	{
+		"title": "Cowboy Bebop",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTySqq1xM0F4LHRuZZYWROkc7ClFCuzBO-2cTB1CIb4cgtI6x1Mq2fDAN3Y468&s"
+	},
+	{
+		"title": "Death Note",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU2PxNF1FaL8RM-skBSbHsJ0Y-9bN956jI9YAkKP1p7KGOZDlFGFYIgE5w2nM&s"
+	},
+	{
+		"title": "Death Parade",
+		"score": "5",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgeN63dJjisKMlVcY6zFMm4_YrccfSS920JQcPWxrpDtwCW_rtitiKzt-QYg&s"
+	},
+	{
+		"title": "Dororo",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFu4b7dNqJX_iao93FBOmZqDkI_q-4bH8azUXMsXHi__Q-wDWFpxf7usPgiw&s"
+	},
+	{
+		"title": "Dragon Ball Super",
+		"score": "6",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIHXQinUW5otNeHV6tO5vflpCK1fzPa2YQXRZG1aGEh4sPuVXLL1BimGqquA&s"
+	},
+	{
+		"title": "Dragon Ball Z",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtGQi95DePOOntxDNv7wCZNWxunuSihUeI3GvAirBmK_VJsMIj4eelmcW2NA&s"
+	},
+	{
+		"title": "Fate/Zero",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwmP9d8I_nUzjBxEuYDN5gRiB_qKDvvAq85av8S7ZiMJhBmU2nQXE9egvA2g&s"
+	},
+	{
+		"title": "Fullmetal Alchemist: Brotherhood",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXLEqfKeMHbKzarOPyee_iMqF0TtsMDtGebnzwVL0dpDUigQ0aN0iohBhb8Hk&s"
+	},
+	{
+		"title": "Ginga Eiyuu Densetsu: Arata Naru Tatakai no Overture",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtPTAtuPedM_bnZIFQ9Dc1SNiPbGKI9_4BvRaHtvKHsH0M5l-1dgGC1tiqbA&s"
+	},
+	{
+		"title": "Ginga Eiyuu Densetsu: Waga Yuku wa Hoshi no Taikai",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFaZLQvPnT19ShqF6xJ-fTSrIZxbzIlVaYA-zfImMeYa9CfvvdxRCEhXLmpg&s"
+	},
+	{
+		"title": "Gintama",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROe5Cz_CK9caQWEwTIZ_kZgbZ9k0acOQ0WTVRgo0TCrKfyKOJuKprTgOF0ssk&s"
+	},
+	{
+		"title": "Gintama Movie 2: Kanketsu-hen - Yorozuya yo Eien Nare",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPpxeI1I45adk9KHo0KYHB31QS4_j_LyqP-c4HQh_R12KACYyIvdjRT0BKCf8&s"
+	},
+	{
+		"title": "Gintama'",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROe5Cz_CK9caQWEwTIZ_kZgbZ9k0acOQ0WTVRgo0TCrKfyKOJuKprTgOF0ssk&s"
+	},
+	{
+		"title": "Gintama': Enchousen",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS96TYCRA757_i8BvFfbwWAPEr9VWCec0JMUpjf2va-TGWK47rUqsxg4RSWhw&s"
+	},
+	{
+		"title": "Gintama.",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROe5Cz_CK9caQWEwTIZ_kZgbZ9k0acOQ0WTVRgo0TCrKfyKOJuKprTgOF0ssk&s"
+	},
+	{
+		"title": "Gintama.: Shirogane no Tamashii-hen",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjlvxn3_PhXkqdnytmIx9pGSUQDPWXJL8a-RDwZGlv7nzkBfsD9kEb1zAnbyc&s"
+	},
+	{
+		"title": "Gintama.: Shirogane no Tamashii-hen - Kouhan-sen",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShsU9jHaFr3l0udJR8fKIaib0Z2UtvGw9OyPPtly6-RPTCiWMX7wWsCOtmpQ&s"
+	},
+	{
+		"title": "Gintama°",
+		"score": "0",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROe5Cz_CK9caQWEwTIZ_kZgbZ9k0acOQ0WTVRgo0TCrKfyKOJuKprTgOF0ssk&s"
+	},
+	{
+		"title": "High School DxD",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKNqOkFWuoTB_yjhBmwd3AhYMJoFIClCxjaN5toNcnUGuY6Oa4kEf1u-HeeA&s"
+	},
+	{
+		"title": "Hunter x Hunter (2011)",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVcgGdvYuDBzrs_HFlNwFRzIz06l3FTK-lUgt6BKv8sqVIuyyiiStPjASanA&s"
+	},
+	{
+		"title": "Inazuma Eleven",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1B8b3-QhsTamRyu8HynXyPfypBBYQZOFiSCC5zxQVke5Fe374XtDId3vRdQ&s"
+	},
+	{
+		"title": "JoJo no Kimyou na Bouken (TV)",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDq4BnM6cSc3PxJh7VupgRqZD2Dwd5-TR2UD71o6mmWk-44Xbp33bF1NWJX2o&s"
+	},
+	{
+		"title": "JoJo no Kimyou na Bouken Part 3: Stardust Crusaders 2nd Season",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0-GZHe1cknNUXDOgLF3W5O8mpjhH1_sist93iXSGT_6CM1xzYFrDr6OWuCLQ&s"
+	},
+	{
+		"title": "Kimetsu no Yaiba",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgrWKC6GBn5DYVgIn4TSNxjUaqTOOSJp57uVWqOmfjv0Iy4B7_WpAQJeBszeE&s"
+	},
+	{
+		"title": "Kimi no Na wa.",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7CKRFjd3RjljFMF44awcRPx7kY0qMcpOtXzDchugCCNqOzIdoBa8zGGTzkUE&s"
+	},
+	{
+		"title": "Kiseijuu: Sei no Kakuritsu",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxFqB1afuU8gyYQBGSFaFTaz5riYDUjWNF8UzEw8O5mHkIdTBHFJ9ksOATzw&s"
+	},
+	{
+		"title": "Koe no Katachi",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmbdS-S4Rcq5Sf3IIziQVXSZTJUPLkkK1ktgq-y9lyPCwCF1IL9c9OcPqtoxc&s"
+	},
+	{
+		"title": "Made in Abyss",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWK75ji-Hl1RyjggHYPV4e7PYv3NJc4IoMDaKdb4YNqk2f06GG4FYfQBR6ugM&s"
+	},
+	{
+		"title": "Mirai Nikki",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPAQn-gozVFv5xNV2BuhI-t-OkHHlMCVI0C7w-A6c6Eghm9WhfIZrfQWQl4g&s"
+	},
+	{
+		"title": "Mob Psycho 100",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUi0Sc-8uMx9MbEJ-WBTltrAyK7l2h-lZs5bu4Tf5q79oHgo4n68DFbjA8SlA&s"
+	},
+	{
+		"title": "Mob Psycho 100 II",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG3c-8TunZIzmcF7Mpy6kHy0JHZGv7jC0QyvxXyqXW1ivjxIG1PjAj3mt9NA&s"
+	},
+	{
+		"title": "Monster",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqajMKsnl_e9pg9WNLj0GV4PYrLEHkrqkVKLgtgEY87KzJ03PmPOFvyRzIYjw&s"
+	},
+	{
+		"title": "Nanatsu no Taizai",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrTayUEQqLlAYQcmtbUfumgLUjCAqF6GVQmODcT1LO5ufoMBPDG2qIO6MdOg&s"
+	},
+	{
+		"title": "Nanatsu no Taizai: Imashime no Fukkatsu",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy5nDEPKaR4FNZ7-7V5OoRvoAdC3sXmIgMF1O1bllUdQMP9fKM6swD4CRjZA&s"
+	},
+	{
+		"title": "Naruto",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4Vc3hiU3oRgrVnPbP98v9EJPNIwZmvBncnX7iKd4tGNaShSjiFY1ZSqjAxks&s"
+	},
+	{
+		"title": "Naruto: Shippuuden",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiqWChuPfEAZBQG57Ym-Z5z9qOXSy7bSuFF69ALpPtRiCSlJtghVO_un1WfAo&s"
+	},
+	{
+		"title": "Neon Genesis Evangelion",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3MxF905qOgK8oZZkdrC37qcJ0tMbs_z6c3kyUj15J2koksPML6UT5RHAgfbs&s"
+	},
+	{
+		"title": "Neon Genesis Evangelion: The End of Evangelion",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbCmdQvAVBnvi_Z7PUhEXeRJYeW7IhOb847wxJ_DcefLzHF0rJZRX7rFWQzA&s"
+	},
+	{
+		"title": "No Game No Life",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx7ZHomFzo6O7kBSpHGBpp1h0JOOhRL4WXGu9p_bYzWhrjhVAJVVik-aqd2_o&s"
+	},
+	{
+		"title": "One Punch Man",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbeCCr9MZainmc07n0U_Ja6iHiP65EFpYF-Xj03mxGpD98iyTLpOmhvltOyTM&s"
+	},
+	{
+		"title": "One Punch Man 2nd Season",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKCSrrZyiVH-JsNUOMn7PDDkypRwh8A7-WYg6O1ziFlpgZ8ypytfBnrZR-rw&s"
+	},
+	{
+		"title": "Prison School",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjk6fYNcdsSz6GGRWUQ59Dwm0DgylqTkKRk44JlfwzAaRBOEXId5ncyCG9Uw&s"
+	},
+	{
+		"title": "Prison School: Mad Wax",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTraQV92YtaBvPfeXW5w-cIv7B420pGtba64io_X6neVMazMCH10KLDs76rcI8&s"
+	},
+	{
+		"title": "Sen to Chihiro no Kamikakushi",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP3TRUKEkLbnOLfB0n__V3b0YInkSGttzBpdXEGowefwK2CAKyKmhMeyW9fb0&s"
+	},
+	{
+		"title": "Shigatsu wa Kimi no Uso",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTteDs-rOptSWoDlPBLXIT6P2EljarqXVsLmAmElYRixBAFL0zxI2wjXAFTXg&s"
+	},
+	{
+		"title": "Shingeki no Kyojin",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFstxMi5Z4gc3vxrCw7edIOa02S08-j8z3W4AK4fEhLg2H7p9J5rStJ5rkow&s"
+	},
+	{
+		"title": "Shingeki no Kyojin Season 2",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2NFsNdVtRHKHMqWkC7tShvMQ_er1Bso_J0LoYXjl7iU3C-_6i4c1lNqVTWWw&s"
+	},
+	{
+		"title": "Shingeki no Kyojin Season 3",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKibyLgUttZ9uU-iKqBMSAGKEk-2nb6K98O_Id46tfduMcVrjAjB6TgIpleNM&s"
+	},
+	{
+		"title": "Shingeki no Kyojin Season 3 Part 2",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdB5juT06-gc1piI3-aWv8TuRK-kgXYSwwWmq9zX5aaa7ZEY-XMDX3VtlytF4&s"
+	},
+	{
+		"title": "Steins;Gate",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaebfimD1YMgkIbM4W5EiY6F05eWf48YYO55pFqsLnLgTA4-yrXiOGE_I65WQ&s"
+	},
+	{
+		"title": "Steins;Gate 0",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN3a5H_fObu-xpN9lM_nKRn9PfEbhbxLO005IUZXYBv_UdKZzQ7npLvBSya8U&s"
+	},
+	{
+		"title": "Steins;Gate: Kyoukaimenjou no Missing Link - Divide By Zero",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT59rb69BONOv2u_CAjXz9eEa7ypibPYI0g010zrR7ieGBNQHyzhX2AxwwGUKw&s"
+	},
+	{
+		"title": "Tenki no Ko",
+		"score": "9",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-faDmvUWxmTEAjEsBMh_jFqKicB_C0C1aq7VnfxRaPra0waB2HqA5UTNGmg&s"
+	},
+	{
+		"title": "Tokyo Ghoul",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGr054xVKLECBqtap157cKtc5aetgYDaarEm5_2h_7g4sxAv6U27YWqdIwiw&s"
+	},
+	{
+		"title": "Tokyo Ghoul √A",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbKXFBRSmSMUXv9dY_wE2-Jyd8T9_ZwYlp3QAuGvwzTcxlXWPkepJvbAdg1pI&s"
+	},
+	{
+		"title": "Tokyo Ghoul:re",
+		"score": "8",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVpxa9qAO1zL2PPlY-SKjlfmQzevZHp5-wUOIRzN_133XWysSvdbPYKP_cVXM&s"
+	},
+	{
+		"title": "Tokyo Ghoul:re 2nd Season",
+		"score": "7",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRToSgl3ltMbGL1yd_BRZL-m5DuR6904A0jkCiFnRqnZbUeBSu6v2j5hWh2Fw&s"
+	},
+	{
+		"title": "Vinland Saga",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9AfIq95bQiwpjggjIQuuOZNN7GosNmjA9il5iDbagf4pznC_fyqnEcq3Ui_k&s"
+	},
+	{
+		"title": "Yakusoku no Neverland",
+		"score": "10",
+		"url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEzJmrCVGmPCNIVPdOV9pyfE_N27Yotf2SOBAdxjVglJ56iVhAD20WRVohEOs&s"
+	}
+]
