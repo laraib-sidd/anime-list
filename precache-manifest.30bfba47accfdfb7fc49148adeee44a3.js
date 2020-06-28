a2d7fe0c82bca34eac4a8cd93b085e4453b83e23
laraib-sidd <laraib.sidd7@gmail.com>
2020-06-28 18:05:30 +0530

    Updates

---
+++ precache-manifest.30bfba47accfdfb7fc49148adeee44a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d96c3a05a3b9aa0d0cf766f812c81072",
+    "url": "/anime-list/index.html"
+  },
+  {
+    "revision": "288da7ee4cd9c876322b",
+    "url": "/anime-list/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "976b56c571c0096a86a0",
+    "url": "/anime-list/static/css/main.487af2b1.chunk.css"
+  },
+  {
+    "revision": "288da7ee4cd9c876322b",
+    "url": "/anime-list/static/js/2.7a34c278.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anime-list/static/js/2.7a34c278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976b56c571c0096a86a0",
+    "url": "/anime-list/static/js/main.89e107f9.chunk.js"
+  },
+  {
+    "revision": "2bb8bf70d1417ed96961",
+    "url": "/anime-list/static/js/runtime-main.3e28973c.js"
+  }
+]);
