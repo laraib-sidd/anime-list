fc3a96c410f80ccce1de69e3e60348d6e6d3407b
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-02 17:00:54 +0530

    update

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -3,7 +3,8 @@ import React from 'react'
 const SearchBox = ({searchChange}) => {
         return (
             <div className='pa2'>
-                <input className="pa3 ba b--green bg-lightest-blue"
+                <input className="pa3 ba"
+                style = {{backgroundColor: '#d4d4d4'}}
                 type="search"
                  placeholder="Search Anime"
                      onChange={searchChange}
